cf7b6843b7f20c75cd1d067b2395197cb1c2cee7
Jon Kjar <kjarjon@gmail.com>
2021-03-01 16:19:44 -0700

    moded life file

--- life_generator.py
+++ life_generator.py
@@ -20,12 +20,16 @@ outputGridVals = []
 
 def main():
     lgg = LifeGeneratorGUI()
+    lgg.create_gui()
+
+
 
 
 class LifeGeneratorGUI:
     entry_gui = None
     type_val = None
     quantity_variable = None
+    prod_types = None
 
     def __init__(self):
         # read the dataset
@@ -34,26 +38,15 @@ class LifeGeneratorGUI:
         if dataset is None:
             print("you have a bad db csv file")
 
-        prod_types = dataset.get_all_types()
+        self.prod_types = dataset.get_all_types()
 
-        # read the input file if exists
+        # run from input csv file and end program
         if len(sys.argv) > 1:
-            input1 = sys.argv[1]
-            input_data = c.InputData(input1)
-            csv_output = []
-            for inputDataRow in input_data.data:
-                print(str(inputDataRow))
-                result_array = get_top(inputDataRow.inputCat, int(inputDataRow.inputNumToGen))
-                print(str(result_array))
-                results = []
-                for resultRow in result_array:
-                    results.append(resultRow.create_csv_line(inputDataRow))
-                csv_output.extend(results)
-            c.create_csv(csv_output)
-
-            # break so GUI does not open
+            self.run_batch()
             return
 
+
+    def create_gui(self):
         # setup the window
         root1 = tk.Tk()
 
@@ -63,8 +56,7 @@ class LifeGeneratorGUI:
         mainframe = tk.Frame(root1)
         mainframe.grid(column=0, row=0)
 
-        vcmd = (root1.register(validate),
-                '%P')
+        vcmd = (root1.register(validate), '%P')
 
         # row 1 - output quantity
         quantity_text = tk.StringVar()
@@ -84,10 +76,9 @@ class LifeGeneratorGUI:
         type_dir.grid(column=1, row=2)
 
         self.type_val = tk.StringVar(mainframe)
-        self.type_val.set(prod_types[0])
+        self.type_val.set(self.prod_types[0])
 
-        type_options = ttk.Combobox(mainframe, textvariable=self.type_val, values=prod_types)
-
+        type_options = ttk.Combobox(mainframe, textvariable=self.type_val, values=self.prod_types)
         type_options.grid(column=2, row=2)
 
         # row 3 - generate
@@ -100,75 +91,57 @@ class LifeGeneratorGUI:
 
         root1.mainloop()
 
+    def __create_entry(self, name, col, row=0):
+        header = tk.Entry(self.entry_gui, width=20)
+        header.grid(row=row, column=col)
+        header.insert(tk.END, name)
+        outputGridVals.append(header)
+
     def generate_click(self):
         input_row = c.InputRowData("toy", self.type_val.get(), self.quantity_variable.get())
 
         result_array = get_top(input_row.inputCat, input_row.inputNumToGen)
 
+        # clear out all old values
         for x in outputGridVals:
             x.destroy()
 
-        tp = tk.Entry(self.entry_gui, width=20)
+        # create headers
+        self.__create_entry(c.PROD_NAME, 4)
+        self.__create_entry(c.AVER_REV, 5)
+        self.__create_entry(c.NUM_REV, 6)
+        self.__create_entry(c.SELLER_STATE, 7)
 
-        header4 = tk.Entry(self.entry_gui, width=20)
-        header4.grid(row=0, column=4)
-        header4.insert(tk.END, c.PROD_NAME)
-        outputGridVals.append(header4)
-
-        header5 = tk.Entry(self.entry_gui, width=20)
-        header5.grid(row=0, column=5)
-        header5.insert(tk.END, c.AVER_REV)
-        outputGridVals.append(header5)
-
-        header6 = tk.Entry(self.entry_gui, width=20)
-        header6.grid(row=0, column=6)
-        header6.insert(tk.END, c.NUM_REV)
-        outputGridVals.append(header6)
-
-        header7 = tk.Entry(self.entry_gui, width=20)
-        header7.grid(row=0, column=7)
-        header7.insert(tk.END, c.SELLER_STATE)
-        outputGridVals.append(header7)
-
-        dic = {}
-        for i, row in enumerate(result_array):
-            dic[row.PROD_NAME] = i
         # receive column 7 data from content generator
-        print(str(dic))
         lg_client = lgc.LifeGenClient(CLIENT_PORT)
-        #lg_client.send_initial_info(dic)
         wiki_desc = lg_client.receive_info()
-        #wiki_desc = ["HEY"] * len(result_array)
-        # del lg_client
 
         # populate the contents of table
         for i, row in enumerate(result_array):
-            tp4 = tk.Entry(self.entry_gui, width=20)
-            tp4.grid(row=i + 1, column=4)
-            tp4.insert(tk.END, row.PROD_NAME)
-            outputGridVals.append(tp4)
-
-            tp5 = tk.Entry(self.entry_gui, width=20)
-            tp5.grid(row=i + 1, column=5)
-            tp5.insert(tk.END, row.AVER_REV)
-            outputGridVals.append(tp5)
-
-            tp6 = tk.Entry(self.entry_gui, width=20)
-            tp6.grid(row=i + 1, column=6)
-            tp6.insert(tk.END, row.NUM_REV)
-            outputGridVals.append(tp6)
-
-            tp7 = tk.Entry(self.entry_gui, width=20)
-            tp7.grid(row=i + 1, column=7)
-            # tp7.insert(tk.END, wiki_desc[row.PROD_NAME])
-            tp7.insert(tk.END, wiki_desc)
-            outputGridVals.append(tp7)
+            self.__create_entry(row.PROD_NAME, 4, i + 1)
+            self.__create_entry(row.AVER_REV, 5, i + 1)
+            self.__create_entry(row.NUM_REV, 6, i + 1)
+            self.__create_entry(wiki_desc, 7, i + 1)
 
         csv_results = []
         for row in result_array:
             csv_results.append(row.create_csv_line(input_row))
 
         c.create_csv(csv_results)
+
+    def run_batch(self):
+        input1 = sys.argv[1]
+        input_data = c.InputData(input1)
+        csv_output = []
+        for inputDataRow in input_data.data:
+            print(str(inputDataRow))
+            result_array = get_top(inputDataRow.inputCat, int(inputDataRow.inputNumToGen))
+            print(str(result_array))
+            results = []
+            for resultRow in result_array:
+                results.append(resultRow.create_csv_line(inputDataRow))
+            csv_output.extend(results)
+        c.create_csv(csv_output)
 
 
 def validate(value_if_allowed):
@@ -177,46 +150,38 @@ def validate(value_if_allowed):
     :param value_if_allowed:
     :return:
     """
-    if value_if_allowed:
-        try:
-            int(value_if_allowed)
-            return True
-        except ValueError:
-            return False
-    else:
+    try:
+        int(value_if_allowed)
+        return True
+    except ValueError:
         return False
 
 
 def get_top(cat, quantity):
     """
-    algorithm to get the top 'quantity' num of products of certain type
+    algorithm to get the top 'quantity' num of products of certain type.
+    Steps:
+    1. get all of category desired
+    2. sort by unique id
+    3. sort by number of reviews
+    4. take top (quantity * 10)
+    5. sort by average review
+    6. take top (quantity)
     :param cat:
     :param quantity:
     :return:
     """
-    # get all of category desired
     p = [s for s in dataset.data if s.CAT_SUB == cat]
 
-    # sort by unique id
     p = sorted(p, key=lambda x: x.ID)
 
-    # sort by number of reviews
     p = sorted(p, key=lambda x: x.NUM_REV, reverse=True)
 
-    # take top (quantity * 10)
     p = p[:(quantity * 10)]
 
-    # sort by average review
     p = sorted(p, key=lambda x: x.AVER_REV, reverse=True)
 
-    # take top (quantity)
     p = p[:quantity]
-
-    # for row in p:
-    #     print('1ID:' + row.ID +
-    #           '\tNum Revs: ' + str(row.NUM_REV) +
-    #           '\tAvg Rev: ' + str(row.AVER_REV) +
-    #           '\tCat: ' + str(row.CAT_SUB))
     return p
 
 
